93dea277b843365849ea53ad096265ee970cdeae
Garrett <sprintepic1122@gmail.com>
2021-05-08 13:01:22 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# test
+
+Open the example project in UE4
+open Content/Geometry/subclass_BP
+crash occurs due to skeletal mesh
---
+++ Source/test/Subclass.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Superclass.h"
+#include "Subclass.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TEST_API ASubclass : public ASuperclass
+{
+	GENERATED_BODY()
+	
+};
